190d5a7dc35b90f993676ff42ae1f17a4e1baffe
Kris Wilson <krispykrisp12@gmail.com>
2019-05-16 16:18:00 -0400

    Add the dealer files

---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+// This is a desk object
+const deck = [{
+    Ace: {
+        aceOfClub: "Ace of club",
+        aceOfSpade: "Ace of spade",
+        aceOfDiamond: "Ace of diamond",
+        aceOfHeart: "Ace of hearts"
+    },
+    Two: {
+        twoOfClub: "Two of club",
+        twoOfSpade: "Two of spade",
+        twoOfDiamond: "Two of diamond",
+        twoOfHeart: "Two of hearts"
+    },
+    Three: {
+        threeOfClub: "Three of club",
+        threeOfSpade: "Three of spade",
+        threeOfDiamond: "Three of diamond",
+        threeOfHeart: "Three of hearts"
+    },
+    Four: {
+        fourOfClub: "Four of club",
+        fourOfSpade: "Four of spade",
+        fourOfDiamond: "Four of diamond",
+        fourOfHeart: "Four of hearts"
+    },
+    Five: {
+        fiveOfClub: "Five of club",
+        fiveOfSpade: "Five of spade",
+        fiveOfDiamond: "Five of diamond",
+        fiveOfHeart: "Five of hearts"
+    },
+    Six: {
+        sixOfClub: "Six of club",
+        sixOfSpade: "Six of spade",
+        sixOfDiamond: "Six of diamond",
+        sixOfHeart: "Six of hearts"
+    },
+    Seven: {
+        sevenOfClub: "Seven of club",
+        sevenOfSpade: "Seven of spade",
+        sevenOfDiamond: "Seven of diamond",
+        sevenOfHeart: "Seven of hearts"
+    },
+    Eight: {
+        eightOfClub: "Eight of club",
+        eightOfSpade: "Eight of spade",
+        eightOfDiamond: "Eight of diamond",
+        eightOfHeart: "Eight of hearts"
+    },
+    Nine: {
+        nineOfClub: "Nine of club",
+        nineOfSpade: "Nine of spade",
+        nineOfDiamond: "Nine of diamond",
+        nineOfHeart: "Nine of hearts"
+    },
+    Ten: {
+        tenOfClub: "Ten of club",
+        tenOfSpade: "Ten of spade",
+        tenOfDiamond: "Ten of diamond",
+        tenOfHeart: "Ten of hearts"
+    },
+    Jack: {
+        jackOfClub: "Jack of club",
+        jackOfSpade: "Jack of spade",
+        jackOfDiamond: "Jack of diamond",
+        jackOfHeart: "Jack of hearts"
+    },
+    Queen: {
+        queenOfClub: "Queen of club",
+        queenOfSpade: "Queen of spade",
+        queenOfDiamond: "Queen of diamond",
+        queenOfHeart: "Queen of hearts"
+    },
+    King: {
+        kingOfClub: "King of club",
+        kingOfSpade: "King of spade",
+        kingOfDiamond: "King of diamond",
+        kingOfHeart: "King of heart"
+    }
+}]
